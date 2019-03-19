caf344b7b5d35be03f78a0003546d0cb3686a4c2
Toralf Wittner <tw@dtex.org>
2019-03-19 15:53:35 +0100

    Update to new yamux implementation.

--- muxers/yamux/src/lib.rs
+++ muxers/yamux/src/lib.rs
@@ -23,53 +23,82 @@
 
 use futures::{future::{self, FutureResult}, prelude::*};
 use libp2p_core::upgrade::{InboundUpgrade, OutboundUpgrade, UpgradeInfo, Negotiated};
-use log::debug;
-use std::{io, iter, sync::atomic};
-use std::io::{Error as IoError};
+use parking_lot::Mutex;
+use std::{io, iter, sync::{Arc, atomic}};
+use tokio_executor::DefaultExecutor;
 use tokio_io::{AsyncRead, AsyncWrite};
 
-// TODO: add documentation and field names
-pub struct Yamux<C>(yamux::Connection<C>, atomic::AtomicBool);
+/// Yamux connection.
+pub struct Yamux<C> {
+    inner: Arc<Mutex<Inner<C>>>,
+    init: atomic::AtomicBool,
+}
+
+struct Inner<C> {
+    handle: yamux::Handle,
+    close: Closing,
+    _mark: std::marker::PhantomData<C>
+}
+
+/// States during connection closing.
+enum Closing {
+    /// Connection is open.
+    NotStarted,
+    /// Connection close is in progress.
+    InProgress(Box<dyn Future<Item = (), Error = io::Error> + Send>),
+    /// All streams have been marked as closed.
+    Done
+}
 
 impl<C> Yamux<C>
 where
-    C: AsyncRead + AsyncWrite + 'static
+    C: AsyncRead + AsyncWrite + Send + 'static
 {
-    pub fn new(c: C, mut cfg: yamux::Config, mode: yamux::Mode) -> Self {
+    pub fn new(c: C, mut cfg: yamux::Config, mode: yamux::Mode) -> Result<Self, io::Error> {
+        let e = DefaultExecutor::current();
+
         cfg.set_read_after_close(false);
-        Yamux(yamux::Connection::new(c, cfg, mode), atomic::AtomicBool::new(false))
-    }
-}
+        let h = yamux::Connection::new(e, c, cfg, mode).map_err(|e| {
+            io::Error::new(io::ErrorKind::Other, e)
+        })?;
+
+        Ok(Yamux {
+            inner: Arc::new(Mutex::new(Inner {
+                handle: h,
+                close: Closing::NotStarted,
+                _mark: std::marker::PhantomData
+            })),
+            init: atomic::AtomicBool::new(false)
+        })
+     }
+ }
 
 impl<C> libp2p_core::StreamMuxer for Yamux<C>
 where
-    C: AsyncRead + AsyncWrite + 'static
+    C: AsyncRead + AsyncWrite + Send + 'static
 {
-    type Substream = yamux::StreamHandle<C>;
-    type OutboundSubstream = FutureResult<Option<Self::Substream>, io::Error>;
-    type Error = IoError;
+    type Substream = yamux::Stream;
+    type OutboundSubstream = Box<dyn Future<Item = Option<Self::Substream>, Error = io::Error> + Send>;
+    type Error = io::Error;
 
-    fn poll_inbound(&self) -> Poll<Self::Substream, IoError> {
-        match self.0.poll() {
-            Err(e) => {
-                debug!("connection error: {}", e);
-                Err(io::Error::new(io::ErrorKind::Other, e))
-            }
+    fn poll_inbound(&self) -> Poll<Self::Substream, io::Error> {
+        match self.inner.lock().handle.poll() {
+            Err(()) => Err(io::Error::new(io::ErrorKind::Other, "connection gone")),
             Ok(Async::NotReady) => Ok(Async::NotReady),
             Ok(Async::Ready(None)) => Err(io::ErrorKind::BrokenPipe.into()),
             Ok(Async::Ready(Some(stream))) => {
-                self.1.store(true, atomic::Ordering::Release);
+                self.init.store(true, atomic::Ordering::Release);
                 Ok(Async::Ready(stream))
             }
         }
     }
 
     fn open_outbound(&self) -> Self::OutboundSubstream {
-        let stream = self.0.open_stream().map_err(|e| io::Error::new(io::ErrorKind::Other, e));
-        future::result(stream)
+        Box::new(self.inner.lock().handle.open_stream()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e)))
     }
 
-    fn poll_outbound(&self, substream: &mut Self::OutboundSubstream) -> Poll<Self::Substream, IoError> {
+    fn poll_outbound(&self, substream: &mut Self::OutboundSubstream) -> Poll<Self::Substream, io::Error> {
         match substream.poll()? {
             Async::Ready(Some(s)) => Ok(Async::Ready(s)),
             Async::Ready(None) => Err(io::ErrorKind::BrokenPipe.into()),
@@ -77,42 +106,61 @@ where
         }
     }
 
-    fn destroy_outbound(&self, _: Self::OutboundSubstream) {
-    }
+    fn destroy_outbound(&self, _: Self::OutboundSubstream) { }
 
-    fn read_substream(&self, sub: &mut Self::Substream, buf: &mut [u8]) -> Poll<usize, IoError> {
+    fn read_substream(&self, sub: &mut Self::Substream, buf: &mut [u8]) -> Poll<usize, io::Error> {
         let result = sub.poll_read(buf);
         if let Ok(Async::Ready(_)) = result {
-            self.1.store(true, atomic::Ordering::Release);
+            self.init.store(true, atomic::Ordering::Release);
         }
         result
     }
 
-    fn write_substream(&self, sub: &mut Self::Substream, buf: &[u8]) -> Poll<usize, IoError> {
+    fn write_substream(&self, sub: &mut Self::Substream, buf: &[u8]) -> Poll<usize, io::Error> {
         sub.poll_write(buf)
     }
 
-    fn flush_substream(&self, sub: &mut Self::Substream) -> Poll<(), IoError> {
+    fn flush_substream(&self, sub: &mut Self::Substream) -> Poll<(), io::Error> {
         sub.poll_flush()
     }
 
-    fn shutdown_substream(&self, sub: &mut Self::Substream) -> Poll<(), IoError> {
+    fn shutdown_substream(&self, sub: &mut Self::Substream) -> Poll<(), io::Error> {
         sub.shutdown()
     }
 
-    fn destroy_substream(&self, _: Self::Substream) {
-    }
+    fn destroy_substream(&self, _: Self::Substream) { }
 
     fn is_remote_acknowledged(&self) -> bool {
-        self.1.load(atomic::Ordering::Acquire)
+        self.init.load(atomic::Ordering::Acquire)
     }
 
-    fn close(&self) -> Poll<(), IoError> {
-        self.0.close().map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    fn close(&self) -> Poll<(), io::Error> {
+        let mut inner = self.inner.lock();
+        match inner.close {
+            Closing::NotStarted => {
+                let mut f = inner.handle.close().map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+                if f.poll()?.is_ready() {
+                    inner.close = Closing::Done;
+                    Ok(Async::Ready(()))
+                } else {
+                    inner.close = Closing::InProgress(Box::new(f));
+                    Ok(Async::NotReady)
+                }
+            }
+            Closing::InProgress(ref mut f) => {
+                if f.poll()?.is_ready() {
+                    inner.close = Closing::Done;
+                    Ok(Async::Ready(()))
+                } else {
+                    Ok(Async::NotReady)
+                }
+            }
+            Closing::Done => Ok(Async::Ready(()))
+        }
     }
 
-    fn flush_all(&self) -> Poll<(), IoError> {
-        self.0.flush().map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    fn flush_all(&self) -> Poll<(), io::Error> {
+        Ok(Async::Ready(()))
     }
 }
 
@@ -142,27 +190,27 @@ impl UpgradeInfo for Config {
 
 impl<C> InboundUpgrade<C> for Config
 where
-    C: AsyncRead + AsyncWrite + 'static,
+    C: AsyncRead + AsyncWrite + Send + 'static
 {
     type Output = Yamux<Negotiated<C>>;
     type Error = io::Error;
     type Future = FutureResult<Yamux<Negotiated<C>>, io::Error>;
 
     fn upgrade_inbound(self, i: Negotiated<C>, _: Self::Info) -> Self::Future {
-        future::ok(Yamux::new(i, self.0, yamux::Mode::Server))
+        future::result(Yamux::new(i, self.0, yamux::Mode::Server))
     }
 }
 
 impl<C> OutboundUpgrade<C> for Config
 where
-    C: AsyncRead + AsyncWrite + 'static,
+    C: AsyncRead + AsyncWrite + Send + 'static
 {
     type Output = Yamux<Negotiated<C>>;
     type Error = io::Error;
     type Future = FutureResult<Yamux<Negotiated<C>>, io::Error>;
 
     fn upgrade_outbound(self, i: Negotiated<C>, _: Self::Info) -> Self::Future {
-        future::ok(Yamux::new(i, self.0, yamux::Mode::Client))
+        future::result(Yamux::new(i, self.0, yamux::Mode::Client))
     }
 }
 
